9dbae29090285c211a2fefc38a97c65ff90ba3be
DylanGrunig <Dylan.G24@k12.xrds.org>
2023-03-20 20:45:22 -0700

    Initial Commit

---
+++ SwiftUICameraExample/SwiftUICameraExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICameraExampleApp.swift
+//  SwiftUICameraExample
+//
+//  Created by Dylan Grunig on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICameraExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
