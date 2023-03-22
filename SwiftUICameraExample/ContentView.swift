e7962b79b0a13a02c36fc7408512f4bb7ab373e2
DylanGrunig <Dylan.G24@k12.xrds.org>
2023-03-21 22:23:04 -0700

    (4Filters)SwiftUIExample

--- SwiftUICameraExample/ContentView.swift
+++ SwiftUICameraExample/ContentView.swift
@@ -1,26 +1,141 @@
-//
-//  ContentView.swift
-//  SwiftUICameraExample
-//
-//  Created by Dylan Grunig on 3/20/23.
-//
-
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    
+    @State var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    
+    @State var selectedImage: UIImage?
+    @State var displayPickerView = false
+    
+    let context = CIContext()
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                if let selectedImage = self.selectedImage{
+                    
+                    Image(uiImage: selectedImage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .frame(width: 300, height: 300)
+                    
+                    
+                } else {
+                    
+                    Image(systemName: "photo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 300, height: 300)
+                    
+                }
+                
+                HStack{
+                    Button(action: {
+                        self.sourceType = .photoLibrary
+                        self.displayPickerView = true
+                    }){
+                        Image(systemName: "folder")
+                            .font(.title)
+                    }.padding()
+                    
+                    
+                    Button(action: {
+                        self.sourceType = .camera
+                        self.displayPickerView = true
+                    }){
+                        Image(systemName: "camera")
+                            .font(.title)
+                        
+                    }.padding()
+                    
+                }
+                
+                HStack{
+                    // Image/buttons
+                    
+                    HStack {
+                        Button("Sepia") {
+                            if let inputImage = selectedImage{
+                                let beginImage = CIImage(image: inputImage)
+                                let currentFilter = CIFilter.sepiaTone()
+                                currentFilter.inputImage = beginImage
+                                currentFilter.intensity = 1
+                                
+                                guard let outputImage = currentFilter.outputImage else { return }
+                                
+                                if let cgImage = context.createCGImage(outputImage, from:
+                                                                        outputImage.extent){
+                                    let uiImage = UIImage(cgImage: cgImage)
+                                    self.selectedImage = uiImage
+                                }
+                            }
+                        }
+                        
+                        Button("Blur") {
+                            if let inputImage = selectedImage {
+                                let beginImage = CIImage(image: inputImage)
+                                let currentFilter = CIFilter.gaussianBlur()
+                                currentFilter.inputImage = beginImage
+                                currentFilter.radius = 10 // adjust the blur radius as needed
+                                
+                                guard let outputImage = currentFilter.outputImage else { return }
+                                
+                                if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                                    let uiImage = UIImage(cgImage: cgImage)
+                                    self.selectedImage = uiImage
+                                }
+                            }
+                        }
+                        
+                        Button("Noir") {
+                            if let inputImage = selectedImage {
+                                let beginImage = CIImage(image: inputImage)
+                                let currentFilter = CIFilter(name: "CIPhotoEffectNoir")
+                                currentFilter?.setValue(beginImage, forKey: kCIInputImageKey)
+                                
+                                guard let outputImage = currentFilter?.outputImage else { return }
+                                
+                                if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                                    let uiImage = UIImage(cgImage: cgImage)
+                                    self.selectedImage = uiImage
+                                }
+                            }
+                        }
+                        
+                        Button("Vignette") {
+                            if let inputImage = selectedImage {
+                                let beginImage = CIImage(image: inputImage)
+                                let currentFilter = CIFilter.vignette()
+                                currentFilter.inputImage = beginImage
+                                currentFilter.intensity = 1
+                                currentFilter.radius = Float(min(inputImage.size.width, inputImage.size.height)) / 2
+                                
+                                guard let outputImage = currentFilter.outputImage else { return }
+                                if let cgImage = context.createCGImage(outputImage, from: outputImage.extent){
+                                    let uiImage = UIImage(cgImage: cgImage)
+                                    self.selectedImage = uiImage
+                                }
+                            }
+                        }
+                        
+                    }
+                    .sheet(isPresented: self.$displayPickerView) {
+                        ImagePickerView(selectedImage: self.$selectedImage, sourceType: self.sourceType)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
-
+                
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+    ContentView()
     }
 }
+                
+
---
+++ SwiftUICameraExample/ImagePickerView.swift
@@ -0,0 +1,52 @@
+//
+//  ImagePickerView.swift
+//  SwiftUICameraExample
+//
+//  Created by Dylan Grunig on 3/20/23.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+
+struct ImagePickerView:UIViewControllerRepresentable{
+
+    @Binding var selectedImage:UIImage?
+    @Environment(\.presentationMode) var isPresented
+    var sourceType: UIImagePickerController.SourceType
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = self.sourceType
+        imagePicker.delegate = context.coordinator
+        
+        return imagePicker
+    }
+    
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator{
+        return Coordinator(picker: self)
+    }
+}
+
+
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    var picker: ImagePickerView
+
+    init(picker: ImagePickerView) {
+        self.picker = picker
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo
+        info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectImage = info[.originalImage] as? UIImage{
+            self.picker.selectedImage = selectImage
+            self.picker.isPresented.wrappedValue.dismiss()
+        }
+    }
+    
+}
